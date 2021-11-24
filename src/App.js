6edadb658c735148c73f10b5b9d1563af6e13a96
viniciosragazzi <viniragazzzi@gmail.com>
2021-11-24 13:12:48 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Welcome from "./pages/welcomePage/welcome";
 import Main from "./pages/mainPage/main";
-
+//Ola
 function App() {
   return (
     <>
